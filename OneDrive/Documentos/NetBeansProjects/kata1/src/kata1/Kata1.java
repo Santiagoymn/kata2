e95bf0a2e79a129e08e20051b72f15a9fc4063bf
ssant <ssant@DESKTOP-PC6CV9R>
2021-09-24 13:30:23 +0100

    Tercera versión de Kata1

--- OneDrive/Documentos/NetBeansProjects/kata1/src/kata1/Kata1.java
+++ OneDrive/Documentos/NetBeansProjects/kata1/src/kata1/Kata1.java
@@ -1,18 +1,14 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.time.*;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Calendar date;
-        date = GregorianCalendar.getInstance();
-        date.set(1949, 9, 23);
         
-        Person person = new Person("Blas", date);
+        LocalDate date2 = LocalDate.of(1970, 9, 23);
+        
+        Person person = new Person("Blas", date2);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
-    }
-    
+    } 
 }
