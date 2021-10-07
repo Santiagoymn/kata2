60c6a5be991496bce8e0684146d53020463aae7d
ssant <ssant@DESKTOP-PC6CV9R>
2021-10-07 16:24:14 +0100

    Primera versión de kata 2

--- OneDrive/Documentos/NetBeansProjects/Kata2/src/kata2/Kata2.java
+++ OneDrive/Documentos/NetBeansProjects/Kata2/src/kata2/Kata2.java
@@ -4,10 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class Kata2 {
-
-
-    public static void main(String[] args) {
+public class Kata2{
+    
+    public static void main(String[] args){
         
         int[] data = {1,2,3,4,5,6,7,8,9,0,1,2,3,4,5};
         
@@ -17,9 +16,9 @@ public class Kata2 {
                 histogram.put(data[i], histogram.get(data[i])+1);
             }else{
                 histogram.put(data[i], 1);
-            }    
+            } 
         }
-        for (Integer key : histogram.keySet()) {
+        for(Integer key : histogram.keySet()){
             System.out.println(key + " ==> " + histogram.get(key));
         }
     }
