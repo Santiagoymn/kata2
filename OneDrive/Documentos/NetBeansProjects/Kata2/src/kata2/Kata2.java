45e5a7c3f8d8974d8bfca21e9367fbb44e1927a2
Santiagoymn <santiagoymn@gmail.com>
2021-10-07 16:37:19 +0100

    Tercera versión de kata 2

--- OneDrive/Documentos/NetBeansProjects/Kata2/src/kata2/Kata2.java
+++ OneDrive/Documentos/NetBeansProjects/Kata2/src/kata2/Kata2.java
@@ -10,13 +10,11 @@ public class Kata2{
         
         int[] data = {1,2,3,4,5,6,7,8,9,0,1,2,3,4,5};
         
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
-    
-        }
-        for(Integer key : histogram.keySet()){
-            System.out.println(key + " ==> " + histogram.get(key));
+        Histogram histo = new Histogram(data);
+        Map<Integer, Integer> histogr = histo.getHistogram();
+        
+        for(Integer key : histogr.keySet()){
+            System.out.println(key + " ==> " + histogr.get(key));
         }
     }
 }
